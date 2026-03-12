fa3ac9a64099f474fd078c0e4a8d5d0d3d55c4c9
MD ZAHED IQBAL <tajiniiuc222@gmail.com>
2026-03-12 15:34:39 +0600

    calculator provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:calculator_app/core/constants/app_theme.dart';
+import 'package:calculator_app/presentation/provider/calculator_provider.dart';
 import 'package:calculator_app/presentation/screen/calculator_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +14,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      home: const CalculatorScreen(),
+    return ChangeNotifierProvider(
+      create: (context) => CalculatorProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        home: const CalculatorScreen(),
+      ),
     );
   }
 }
---
+++ lib/presentation/provider/calculator_provider.dart
@@ -0,0 +1,27 @@
+import 'package:calculator_app/data/model/history_model.dart';
+import 'package:calculator_app/data/service/calculator_service.dart';
+import 'package:flutter/material.dart';
+
+class CalculatorProvider extends ChangeNotifier {
+  String input = "";
+  String result = "0";
+
+  List<HistoryModel> history = [];
+
+  void addInput(String value) {
+    if (value == "=") {
+      result = CalculatorService.calculate(input);
+
+      history.add(HistoryModel(expression: input, result: result));
+
+      input = result;
+    } else if (value == "C") {
+      input = "";
+      result = "0";
+    } else {
+      input += value;
+    }
+
+    notifyListeners();
+  }
+}
