fe6c489d2b91bdfd6f4347cce2022e73283dc3f8
MD ZAHED IQBAL <tajiniiuc222@gmail.com>
2026-05-04 12:20:08 +0600

    few updated

--- lib/presentation/screen/splash_screen.dart
+++ lib/presentation/screen/splash_screen.dart
@@ -15,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    Timer(const Duration(seconds: 4), () {
+    Timer(const Duration(seconds: 1), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const CalculatorScreen()),
